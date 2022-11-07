49bcea332c52a0b655178371ff620728bb719d60
Mario Cotumaccio <mariocotumaccio1@gmail.com>
2022-11-07 11:13:34 +0100

    Creating application.yml

---
+++ src/main/java/com/example/DeployEnvironments02/DeployEnvironments02Application.java
@@ -0,0 +1,13 @@
+package com.example.DeployEnvironments02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployEnvironments02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployEnvironments02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DeployEnvironments02/DeployEnvironments02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DeployEnvironments02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployEnvironments02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
